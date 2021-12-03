d4e99575936ab1f57d556e838bceb88d53a83517
Patrik Laiho <36800055+plaiho@users.noreply.github.com>
2021-12-03 13:25:31 +0200

    Wrap parts in function to avoid repeats

--- 03/part2.jl
+++ 03/part2.jl
@@ -1,7 +1,17 @@
 using DelimitedFiles: readdlm
 
-function discard_row_by_bit_criterion(m, bit, n)
-    return view(m, vec(mapslices(row -> row[n] != bit, m, dims = 2)), :)
+function get_rating(m, majority)
+    
+    function discard_row_by_bit_criterion(m, bit, n)
+        return view(m, vec(mapslices(row -> row[n] != bit, m, dims = 2)), :)
+    end
+
+    for i in 1:size(m,2)
+        majority ? majorities = sum(m, dims=1) .>= size(m)[1]/2 : majorities = sum(m, dims=1) .< size(m)[1]/2
+        if (size(m)[1] > 1) m = discard_row_by_bit_criterion(m, majorities[i], i) end
+    end
+
+    return parse(Int, filter(x -> !isspace(x), bitstring(m.== 1)), base=2)
 end
 
 readings = readdlm(ARGS[1], String)
@@ -9,16 +19,7 @@ readings = readdlm(ARGS[1], String)
 m = map(row -> collect(row), readings)
 m = transpose(UInt32.(reduce(hcat, m)).-48)
 
-mO2 = m; mCO2 = m
-
-for i in 1:size(m,2)
-    majoritiesO2 = sum(mO2, dims=1) .>= size(mO2)[1]/2
-    majoritiesCO2 = sum(mCO2, dims=1) .< size(mCO2)[1]/2
-    if (size(mO2)[1] > 1) global mO2 = discard_row_by_bit_criterion(mO2, majoritiesO2[i], i) end
-    if (size(mCO2)[1] > 1) global mCO2 = discard_row_by_bit_criterion(mCO2, majoritiesCO2[i], i) end
-end
-
-O2rating = parse(Int, filter(x -> !isspace(x), bitstring(mO2.== 1)), base=2)
-CO2rating = parse(Int, filter(x -> !isspace(x), bitstring(mCO2.== 1)), base=2)
-
+O2rating = get_rating(m, true)
+CO2rating = get_rating(m, false)
+ 
 println(O2rating*CO2rating)
