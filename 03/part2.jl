536b4ef9574dd2c018ebbfdc50106cf65e3ce4a3
Patrik Laiho <36800055+plaiho@users.noreply.github.com>
2021-12-03 12:07:38 +0200

    Refactor to use function

--- 03/part2.jl
+++ 03/part2.jl
@@ -9,14 +9,13 @@ readings = readdlm(ARGS[1], String)
 m = map(row -> collect(row), readings)
 m = transpose(UInt32.(reduce(hcat, m)).-48)
 
-mO2 = m
-mCO2 = m
+mO2 = m; mCO2 = m
 
 for i in 1:size(m,2)
     majoritiesO2 = sum(mO2, dims=1) .>= size(mO2)[1]/2
-    majoritiesCO2 = sum(mCO2, dims=1) .>= size(mCO2)[1]/2
-    if (size(mO2)[1] > 1) global mO2 = view(mO2, vec(mapslices(row -> row[i] == majoritiesO2[i], mO2, dims = 2)), :) end
-    if (size(mCO2)[1] > 1) global mCO2 = view(mCO2, vec(mapslices(row -> row[i] != majoritiesCO2[i], mCO2, dims = 2)), :) end
+    majoritiesCO2 = sum(mCO2, dims=1) .< size(mCO2)[1]/2
+    if (size(mO2)[1] > 1) global mO2 = discard_row_by_bit_criterion(mO2, majoritiesO2[i], i) end
+    if (size(mCO2)[1] > 1) global mCO2 = discard_row_by_bit_criterion(mCO2, majoritiesCO2[i], i) end
 end
 
 O2rating = parse(Int, filter(x -> !isspace(x), bitstring(mO2.== 1)), base=2)
