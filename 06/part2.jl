7fa5fa8f5d57310689cabb0bde0044814ac8d263
Patrik Laiho <36800055+plaiho@users.noreply.github.com>
2021-12-06 14:33:34 +0200

    Day 6 solution

---
+++ 06/part2.jl
@@ -0,0 +1,6 @@
+using DelimitedFiles: readdlm
+
+fish = readdlm(ARGS[1], ',', Int32, '\t')
+rot = [[0 1 0 0 0 0 0 0 0]; [0 0 1 0 0 0 0 0 0]; [0 0 0 1 0 0 0 0 0]; [0 0 0 0 1 0 0 0 0]; [0 0 0 0 0 1 0 0 0]; [0 0 0 0 0 0 1 0 0]; [1 0 0 0 0 0 0 1 0]; [0 0 0 0 0 0 0 0 1]; [1 0 0 0 0 0 0 0 0]]
+counts = [count(==(i), fish) for i in [0, 1, 2, 3, 4, 5, 6, 7, 8]]
+println(sum(rot^256*counts))
